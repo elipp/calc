a1e5297d264d7895af81df8ef52b7a86eaaf3c42
Elias <eliaspmiettinen@gmail.com>
2013-10-31 23:22:51 +0200

    removed bad comments

--- include/rl_emul.h
+++ include/rl_emul.h
@@ -1,13 +1,6 @@
 #ifndef RL_EMUL_H
 #define RL_EMUL_H
 
-/* readline emulation for ANSI X3.64-compatible terminal emulators 
-   (to remove the GPL'd GNU readline dependency) 
-   - emulation for emulators. :() */
-
-/* THIS IS NOT A FORK OF THE GNU READLINE LIBRARY, ONLY IMPLEMENTS 
- * A SMALL SUBSET OF THE LINE-EDITING FUNCTIONALITY PROVIDED THEREIN. */
-
 #include <string.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -18,7 +11,7 @@
 void rl_emul_init();
 void rl_emul_deinit();
 
-// these are really the only two *features* we need
+// these are really the only two features we need
 
 char *rl_emul_readline(const char* prompt);
 void rl_emul_hist_add(const char *term);
